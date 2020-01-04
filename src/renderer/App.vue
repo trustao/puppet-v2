de8f7b1d3ae03bb46cbbe28c7bcb009af71e564b
trustao <trustao@yeah.net>
2020-01-04 23:47:47 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'puppet'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
